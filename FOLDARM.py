b53a313bf12567ef097e0a62f29f4a8fa804d26a
gaeun-yang <20215524@sch.ac.kr>
2025-01-06 14:47:06 +0900

    Update FOLDARM.py

--- FOLDARM.py
+++ FOLDARM.py
@@ -11,16 +11,16 @@ BUFFER_SIZE = 1024
 def fold_arm():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.settimeout(5)  
+            client_socket.settimeout(5) 
             client_socket.connect((HOST, PORT))  
             
             fold_command = f"{TSM_ID} {FOLDARM_CMD}{END_MARK}"
-            client_socket.sendall(fold_command.encode())  
+            client_socket.sendall(fold_command.encode())
         
             data = client_socket.recv(BUFFER_SIZE)
             response = data.decode()
         
-            if response.startswith(TSM_ID + FOLDARM_CMD):
+            if response.startswith(TSM_ID + "0"):
                 print("Robot arms folded to ready position.")
             else:
                 print("Error in folding robot arms.")
@@ -34,6 +34,4 @@ def fold_arm():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-fold_arm
-
-fold_arm()
+fold_arm()  
