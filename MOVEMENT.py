42d1c57fe935a6bebacf6804ea4039857f724193
gaeun-yang <20215524@sch.ac.kr>
2025-01-15 16:43:09 +0900

    Update MOVEMENT.py

--- MOVEMENT.py
+++ MOVEMENT.py
@@ -7,9 +7,11 @@ def send_tcp_message(host, port, message):
             s.connect((host, port))
             response = s.recv(1024).decode()
             print(f"Received: {response}")
-            if response.filter("READY"):
+            if response.("READY"):
+              print(f"Response filterd: {"READY","response}")
+            else:
               print(f"Response: {response}")
-
+        
             s.sendall(message.encode())
             print(f"Sent: {message}")
 
@@ -26,7 +28,7 @@ def get_slot1_from_cm1(host, port):
     send_tcp_message(host, port, message)
 
 def put_slot1_to_cm2_slot1(host, port):
-    message = "FDC CM2_SLOT1_PUT#"
+    message = "FDC CM2_SLOT2_PUT#"
     send_tcp_message(host, port, message)
 
 def main():
@@ -38,10 +40,11 @@ def main():
 
     time.sleep(1)
 
-    print("Putting SLOT1 to CM2's SLOT1...")
+    print("Putting SLOT1 to CM2's SLOT2...")
     put_slot1_to_cm2_slot1(host, port)
 
-    print("SLOT1 has been transferred to SLOT1.")
+    print("SLOT1 has been transferred to SLOT2.")
 
 if __name__ == "__main__":
-    main()
+     main()
+     
